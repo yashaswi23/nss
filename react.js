7b068f70a116761bbeca8413b5dec7d8ca6b550a
yashaswi23 <75798032+yashaswi23@users.noreply.github.com>
2021-02-27 18:53:11 +0530

    Create react.js

---
+++ react.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+    	<div>
+            <input onClick>alert("your response has been saved!!");    	 
+    	  
+    	</div>
+    );
+  }
+}
+
+export default App;
